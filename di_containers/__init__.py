24991798f36931745961e6235f4378fe1c5defae
Cinxgler Mariaca Minda <cinxgler@workera.ai>
2021-11-16 09:49:52 -0500

    Removing redundant DI Containers

--- di_containers/__init__.py
+++ di_containers/__init__.py
@@ -1,17 +1,14 @@
 from dependency_injector import containers, providers
 
 from inviter.io import SendInvite
-from inviter.repository import PersonRepositoryJson, PersonRepository
+from inviter.repository import PersonRepositoryJson
+
+__all__ = ["Repositories", "IoAdapters"]
 
 
 class Repositories(containers.DeclarativeContainer):
     Person = providers.Singleton(PersonRepositoryJson, "people.json")
 
-class TestRepositories(containers.DeclarativeContainer):
-    Person = providers.Singleton(PersonRepository)
 
 class IoAdapters(containers.DeclarativeContainer):
-    send_invite = providers.Singleton(SendInvite)
-
-class TestIoAdapters(containers.DeclarativeContainer):
     send_invite = providers.Singleton(SendInvite)
